142e1c4d2dff14976451a64748de9f8aa2a3eba0
Nicola-Perera <nicola.perera96@mail.com>
2021-11-15 12:57:18 +0100

    Create Card Layout common for all the tracks

---
+++ src/components/CardLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="CardLayout">
+        <div class="poster">
+            <img :src="poster" alt="">
+        </div>
+
+        <h2> {{ title }} </h2>
+        <h3> {{ author }} </h3>
+        <h3> {{ year }} </h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CardLayout',
+}
+</script>
+
+<style lang="scss">
+
+</style>
