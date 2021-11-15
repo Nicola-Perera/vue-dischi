ec16942e2d15f02af98ef043e76a50970ca29541
Nicola-Perera <nicola.perera96@mail.com>
2021-11-15 18:09:25 +0100

    fix data import

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -2,7 +2,7 @@
   <div id="SiteMain">
     <div class="songs_library" v-if="!loading">
       <CardLayout
-        v-for="song in response"
+        v-for="song in songs"
         class="card brend_light_grey"
         :poster="song.poster"
         :title="song.title"
@@ -10,6 +10,11 @@
         :year="song.year"
         :key="song.title"
       />
+    </div>
+
+    <!-- loading screen -->
+    <div class="loading" v-else>
+      <h1>Loading...</h1>
     </div>
   </div>
 </template>
@@ -31,20 +36,18 @@ export default {
     };
   },
 
-  methods: {
-    callApi() {
-      axios
-        .get('https://flynn.boolean.careers/exercises/api/array/music')
-        .then((Response) => {
-          console.log(Response.data);
-          this.songs = Response.data;
-          this.loading = false;
-        })
-        .catch((Error) => {
-          console.log(Error, 'ERRORE!');
-          this.error = `ERRORE ${Error}`;
-        });
-    },
+  mounted() {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((Response) => {
+        console.log(Response.data);
+        this.songs = Response.data.response;
+        this.loading = false;
+      })
+      .catch((Error) => {
+        console.log(Error, 'ERRORE!');
+        this.error = `ERRORE ${Error}`;
+      });
   },
 };
 </script>
