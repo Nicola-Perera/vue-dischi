cf410b604897de700295e0802b1f6902d1ca5bff
Nicola-Perera <nicola.perera96@mail.com>
2021-11-16 19:31:03 +0100

    swap filter component from checkbox to option menu

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,37 +1,25 @@
 <template>
-    <div id="genre_filter">
+    <div id="filter_box">
+        
+        <select name="genre_filter" id="genre_filter">
+            <option value="all">all</option>
+            <option value="rock">rock</option>
+            <option value="pop">pop</option>
+            <option value="jazz">jazz</option>
+            <option value="metal">metal</option>
+        </select>
 
-        <div class="checkbox">
-            <input type="checkbox" name="all_genre" id="all_genre">All
-        </div>
-
-        <div class="checkbox">
-            <input type="checkbox" name="rock_genre" id="rock_genre">Rock
-        </div>
-
-        <div class="checkbox">
-            <input type="checkbox" name="pop_genre" id="pop_genre">Pop
-        </div>
-
-        <div class="checkbox">
-            <input type="checkbox" name="jazz_genre" id="jazz_genre">Jazz
-        </div>
-
-        <div class="checkbox">
-            <input type="checkbox" name="metal_genre" id="metal_genre">Metal
-        </div>
     </div>
 </template>
 
 <script>
-
-export default {
-    data() {
-        return {
-
-        }
-    },
-}
+    export default {
+        data() {
+            return {
+                filterGenre:'',
+            };
+        },
+    };
 </script>
 
 <style lang="scss">
