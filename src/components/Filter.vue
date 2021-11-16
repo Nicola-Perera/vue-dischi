27c8cf8474a397a8fadcd8424400c516a8edcc52
Nicola-Perera <nicola.perera96@mail.com>
2021-11-16 12:51:43 +0100

    create Filter component

---
+++ src/components/Filter.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="genre_filter">
+
+        <div class="checkbox">
+            <input type="checkbox" name="all_genre" id="all_genre">All
+        </div>
+
+        <div class="checkbox">
+            <input type="checkbox" name="rock_genre" id="rock_genre">Rock
+        </div>
+
+        <div class="checkbox">
+            <input type="checkbox" name="pop_genre" id="pop_genre">Pop
+        </div>
+
+        <div class="checkbox">
+            <input type="checkbox" name="jazz_genre" id="jazz_genre">Jazz
+        </div>
+
+        <div class="checkbox">
+            <input type="checkbox" name="metal_genre" id="metal_genre">Metal
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+
+</style>
